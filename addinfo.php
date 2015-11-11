952ba27fcf971cfb003ef5320df8d4371a62b0ae
linux-fox <linux-fox@163.com>
2015-11-11 15:50:22 +0800

    add addinfo.php

---
+++ addinfo.php
@@ -0,0 +1,22 @@
+<?php
+require 'includes/conn.php';
+
+$sender = $mysqli->real_escape_string($_POST['sender']);
+$receiver = $mysqli->real_escape_string($_POST['receiver']);
+$time = $mysqli->real_escape_string($_POST['time']);
+$location = $mysqli->real_escape_string($_POST['location']);
+$moneyamount = $mysqli->real_escape_string($_POST['moneyamount']);
+$description = $mysqli->real_escape_string($_POST['description']);
+
+$sql = "INSERT INTO activityinfo(sender,receiver,time,location,moneyQuantity,description) VALUES('$sender','$receiver','$time','$location','$moneyamount','$description')";
+
+if (!$mysqli->query($sql)) {
+    printf("Error: %s\n", $mysqli->error);
+    exit();
+}
+
+echo "<script>alert('添加成功！')</script>";
+echo "<script>location='index.html'</script>";
+
+$mysqli->close();
+?>
