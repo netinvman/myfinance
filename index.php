71c4bf214175d4c0cb99e012bd1eccd575f9b3f5
linux-fox <linux-fox@163.com>
2015-11-12 08:27:50 +0800

    add index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>随礼记录管理程序</title>
+    </head>
+    <body>
+    <table border='0' cellspacing='0' align='center'>
+        <caption><h2>记录查询|<a href='addinfo.html'>添加记录</a></h2></caption>
+        <form action='showinfo.php' method='get'>
+            <tr>
+            <td>随礼人:<input type='text' name='sender'></td>
+            </tr>
+            <tr>
+            <td>接收人:<input type='text' name='receiver'></td>
+            </tr>
+            <tr>
+            <td><input type='submit' value='查询'> <input type='reset' value='重置'></td>
+            </tr>
+        </form>
+    </table>
+    </body>
+</html>
